d407b7f7bcd9e32d081136d251cdc28831fdc93a
Rafael do carmo <rafaelcarmo_15@hotmail.com>
2021-02-10 20:20:07 -0300

    info-content

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 import Header from './components/header'
-
+import Image from 'next/image'
 
 
 export default function Home() {
@@ -25,13 +25,13 @@ export default function Home() {
       <div>
         <div className="infoWho">
         <h3>  Quem sou eu? </h3>
-        <h5>{process.env.WHO_I}</h5>
+        <h5>{process.env.NEXT_PUBLIC_WHO_I}</h5>
         </div>
 
       <div></div>
 
         <div className="infoDescription">
-        <p>{process.env.DESCRIPTION}</p>
+        <p>{process.env.NEXT_PUBLIC_DESCRIPTION}</p>
         </div>
 
         <div>
@@ -43,17 +43,70 @@ export default function Home() {
         <ul>
 
             <li>
-              Conteudo 1
+    
+
+                  <div className="topNumber_1">
+
+                    <div className="intoNumber_1">
+                          <h3 className="number_1">01</h3> <p>Noivas</p>
+                    </div>
+
+     
+                    </div>             
+             
+
+                  <div className="content1">
+                    <p>{process.env.NEXT_PUBLIC_INFO1}</p>
+                  </div>
+                  
+             <div className="image1">
+                    <Image className="image1" src="/noiva.png" alt="Noivas" width={130} height={200} /> 
+                    </div>
             </li>
             
 
             <li>
-              Conteudo 2
+      <div className="image1">
+                    <Image className="image1" src="/formanda.png" alt="Formandas" width={130} height={200} /> 
+                    </div>
+                  <div className="topNumber_1">
+
+                    <div className="intoNumber_1">
+                          <h3 className="number_1">02</h3> <p>Formandas </p>
+                    </div>
+
+     
+                    </div>             
+             
+
+                  <div className="content1">
+                    <p>{process.env.NEXT_PUBLIC_INFO1}</p>
+                  </div>
+                  
+       
             </li>
             
 
             <li>
-              Conteudo 3
+    
+
+                  <div className="topNumber_1">
+
+                    <div className="intoNumber_1">
+                          <h3 className="number_1">03</h3> <p>Artísticas</p>
+                    </div>
+
+     
+                    </div>             
+             
+
+                  <div className="content1">
+                    <p>{process.env.NEXT_PUBLIC_INFO1}</p>
+                  </div>
+                  
+             <div className="image1">
+                    <Image className="image1" src="/artistica.png" alt="Noivas" width={130} height={200} /> 
+                    </div>
             </li>
             
 
