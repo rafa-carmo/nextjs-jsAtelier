b8a6eb0ce539e5e061e4ae2be713d876d6e61d5c
Rafael do carmo <rafaelcarmo_15@hotmail.com>
2021-03-15 22:31:30 -0300

    fix 'fs' bug

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  },
     images: {
         domains: ['i.ibb.co'],
-      },
-      webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-          config.node = {
-            fs: 'empty'
-          }
-        }
-    
-        return config
       }
     
 }
